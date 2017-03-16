d45787b098d67124d07dc1c0ca64d62e45fdd5d6
Paul Williams <paul.williams@menziesdistribution.com>
2017-03-16 16:09:46 +0000

    Back navigation and BaseController.js

--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -2,9 +2,9 @@ sap.ui.define([
    "sap/ui/demo/nav/controller/BaseController"
 ], function (BaseController) {
    "use strict";
-
-   return BaseController.extend("sap.ui.demo.nav.controller.Home", {
-
+   return BaseController.extend("sap.ui.demo.nav.controller.NotFound", {
+      onInit: function () {
+      }
    });
-
 });
+
